1df09cb13a69a5bb21b1049b6306dcbff5fee13f
hdkid7 <hameddiakite34@yahoo.com>
2021-04-29 14:31:23 -0400

    added a number to word conversion dictionary

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const userArguments = process.argv.slice(2)
 
+const numToWord = {
+    0:"Zero",
+    1:"One",
+    2:"Two",
+    3:"Three",
+    4:"Four",
+    5:"Five",
+    6:"Six",
+    7:"Seven",
+    8:"Eight",
+    9:"Nine"
+}
 
 
 
